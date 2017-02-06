d9c7935a473599ba3eaa9c711d57c30d43c602d4
Nima Boscarino <nima.boscarino@gmail.com>
2017-02-06 02:18:52 -0800

    split into partial views, collection page shows tiled pictures for collection

--- server.js
+++ server.js
@@ -2,62 +2,68 @@ var express = require('express');
 var formidable = require('formidable');
 var util = require('util');
 var fs = require('fs-extra');
+var pug = require('pug');
 
 var app = express();
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  const pug = require('pug');
-
   // Compile template.pug, and render a set of data
   var page = pug.renderFile('views/index.pug');
 
   res.send(page);
-})
+});
+
+app.get('/collections/:name', function (req, res) {
+  const folder = 'public/uploads/' + req.params.name + '/';
+  fs.readdir(folder, (err, files) => {
+    var page = pug.renderFile('views/collection.pug', {
+      name: req.params.name,
+      files: files
+    });
+    res.send(page);
+  })
+
+
+});
 
 app.post('/upload', function(req, res){
-    var form = new formidable.IncomingForm(),
-    files = [],
-    fields = [];
-    form.on('field', function(field, value) {
-      console.log(field, value);
-        fields.push([field, value]);
+  var form = new formidable.IncomingForm();
+  var files = [];
+  var fields = [];
+  form.on('field', function(field, value) {
+    fields[field] = value;
+  })
+  form.on('file', function(field, file) {
+    files.push(file);
+  })
+
+  form.on('error', function(err) {
+    console.error(err);
+  });
+
+  form.on('end', function() {
+    console.log(fields);
+    var new_location = `public/uploads/${fields.collectionName}/`;
+    fs.ensureDir(new_location, function (err) {
+      files.forEach(function(file) {
+        var temp_path = file.path;
+        var file_name = file.name;
+
+        fs.copy(temp_path, new_location + file_name, function(err) {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(file_name)
+            }
+        });
+      });
     })
-    form.on('file', function(field, file) {
-        console.log(file.name);
-        files.push([field, file]);
-    })
+    res.redirect('/collections/' + fields.collectionName);
+  });
 
-    form.on('progress', function(bytesReceived, bytesExpected) {
-        var percent_complete = (bytesReceived / bytesExpected) * 100;
-        console.log(percent_complete.toFixed(2));
-    });
-
-    form.on('error', function(err) {
-        console.error(err);
-    });
-
-    form.on('end', function(fields, files) {
-        var new_location = 'public/uploads/';
-
-        this.openedFiles.forEach(function(openedFile) {
-          var temp_path = openedFile.path;
-          var file_name = openedFile.name;
-
-          fs.copy(temp_path, new_location + file_name, function(err) {
-              if (err) {
-                  console.error(err);
-              } else {
-                  console.log("success!")
-              }
-          });
-
-        })
-        res.redirect('/');
-    });
-
-    form.parse(req);
+  form.parse(req);
 });
 
 
